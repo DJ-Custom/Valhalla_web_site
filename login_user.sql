abe2d570761d0fad08c81e9f6bd008381ee24925
Herley David Alpizar <herley.david.alpizar@gmail.com>
2025-01-24 15:54:42 -0600

    Delete login_user.sql

--- login_user.sql
+++
@@ -1,4 +0,0 @@
--- Updating user "David" with the correct hashed password
-UPDATE `usuarios` 
-SET `clave` = '$2y$10$e0MYz1Q1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1Z1' 
-WHERE `usuario` = 'David';
